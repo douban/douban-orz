2d076d57dde8f4a43625067ff48ae7347fb2a6c9
phay <moonsolo@gmail.com>
2013-11-20 17:24:37 +0800

    fix version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 # package meta info
 NAME = "ORZ"
-VERSION = "0.2.4.2"
+VERSION = "0.2.4.4"
 DESCRIPTION = "missing data manager in shire, even in middle earth"
 AUTHOR = "fuyuquan"
 AUTHOR_EMAIL = "fuyuquan@douban.com"
