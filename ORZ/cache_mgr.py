7c1363f028ece692f161f2c7f94c61aed0a260f1
phay <moonsolo@gmail.com>
2013-12-25 14:05:08 +0800

    callback workaround

--- ORZ/cache_mgr.py
+++ ORZ/cache_mgr.py
@@ -196,21 +196,25 @@ def _split_dictonary(di, predicate):
     return include_kw, exclude_kw
 
 
-def method_combine(func, reserved_args=tuple()):
+def method_combine(func, reserved_args=tuple(), alias=None):
+    alias = alias if alias is not None else func.func_name
     def _combine(cls_or_self, **kw):
         # 参数的问题没有想清楚，所以下面有些BadSmell
         reserved_kw, exclude_kw = _split_dictonary(kw, lambda k, _: k in reserved_args)
         def call_after(belonged):
-            if hasattr(belonged, "after_"+func.func_name):
-                after_func = getattr(belonged, "after_"+func.func_name)
+            if hasattr(belonged, "after_"+alias):
+                after_func = getattr(belonged, "after_"+alias)
                 after_func(**exclude_kw)
 
-        if hasattr(cls_or_self, "before_"+func.func_name):
-            before_func = getattr(cls_or_self, "before_"+func.func_name)
+        if hasattr(cls_or_self, "before_"+alias):
+            before_func = getattr(cls_or_self, "before_"+alias)
             before_func(**kw)
 
         ret = func(cls_or_self, **reserved_kw)
-        call_after(cls_or_self if ret is None else ret)
+        if alias == 'create':
+            call_after(ret)
+        else:
+            call_after(cls_or_self)
         return ret
     return _combine
 
@@ -247,9 +251,9 @@ def cached_wrapper(cls, table_name, sqlstore=None, mc=None, cache_ver='', id2str
     cls.save = method_combine(save)
     cls.create = classmethod(method_combine(create, db_fields))
     cls.delete = method_combine(delete)
-    cls.delete_transactionally = delete_transactionally
-    cls.create_transactionally = classmethod(create_transactionally)
-    cls.save_transactionally = save_transactionally
+    cls.delete_transactionally = method_combine(delete_transactionally, alias='delete')
+    cls.create_transactionally = classmethod(method_combine(create_transactionally, db_fields, alias='create'))
+    cls.save_transactionally = method_combine(save_transactionally, alias='save')
     cls.__org_init__ = cls.__init__
     cls.__init__ = init
     cls.__setstate__ = setstate
